380c7e74cc4185543a36cec2a43b3d9e0262504c
devgabmal <devgabmal@gmail.com>
2022-10-01 20:14:50 -0300

    Cria componente Cabecalho

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <Cabecalho />
     </div>
+  );
+}
+
+function Cabecalho() {
+  return (
+    <header>
+      Corte madeira. Construa Casas.
+      Contrate trabalhadores. Expanda.
+    </header>
   );
 }
 
