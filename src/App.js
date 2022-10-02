91692fcbfae23544e53be02ceeba732a9d510bb5
devgabmal <devgabmal@gmail.com>
2022-10-01 21:07:45 -0300

    Cria componente Recursos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho/Cabecalho.js';
+import Recursos from './components/Recursos/Recursos.js';
 
 export default function App() {
   return (
     <div className="App">
       <Cabecalho />
+      <Recursos />
     </div>
   );
 }
---
+++ src/components/Recursos/Recursos.js
@@ -0,0 +1,8 @@
+import './Recursos.css';
+
+export default function Recursos() {
+    return (
+        <section className='Recursos'>
+        </section>
+    );
+}
