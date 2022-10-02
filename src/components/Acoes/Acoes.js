cc7db8d87dbfa400240bb2cdfdfe5d6e77b00766
devgabmal <devgabmal@gmail.com>
2022-10-02 00:02:00 -0300

    Torna Acao flexível para várias trocas

--- src/components/Acoes/Acoes.js
+++ src/components/Acoes/Acoes.js
@@ -4,15 +4,45 @@ export default function Acoes() {
     return (
         <section className='Acoes'>
             <Acao nome="Cortar madeira" trocas={[{ recurso: "madeira", valor: 1 }]} />
+            <Acao nome="Vender madeira" trocas={[{ recurso: "madeira", valor: -1 }, { recurso: "ouro", valor: +1 }]} />
         </section>
     );
 }
 
 function Acao(props) {
+    const trocas = props.trocas;
+    const listaTrocas = [];
+
+    let i = 0;
+    for (const troca of trocas) {
+        let texto = "";
+        const recurso = troca.recurso;
+        const valor = troca.valor;
+
+        // Adiciona sinal de + para valores positivos
+        if (valor > 0) {
+            texto += "+";
+        }
+
+        texto += valor + " " + recurso;
+
+        // Adiciona plural conforme necessário
+        if (valor !== 1 && valor !== -1) {
+            if (recurso === "trabalhador") {
+                texto += "e"
+            }
+            texto += "s"
+        }
+
+        // Adiciona elemento à lista
+        listaTrocas.push(<p key={i}>{texto}</p>);
+        i++;
+    }
+
     return (
         <div className='Acao'>
             <button>{props.nome}</button>
-            <p>{props.valores[0].valor + " " + props.valores[0].recurso}</p>
+            <div>{listaTrocas}</div>
         </div>
     );
 }
