ea9b298676d8f0c0db003bb566a43f7af4916ab3
devgabmal <devgabmal@gmail.com>
2022-10-01 22:18:04 -0300

    Cria componente Recurso

--- src/components/Recursos/Recursos.js
+++ src/components/Recursos/Recursos.js
@@ -3,6 +3,17 @@ import './Recursos.css';
 export default function Recursos() {
     return (
         <section className='Recursos'>
+            <Recurso />
         </section>
+    );
+}
+
+function Recurso() {
+    return (
+        <div className='Recurso'>
+            <h1>Madeira</h1>
+            <span>🌲</span>
+            <p>10000</p>
+        </div>
     );
 }
